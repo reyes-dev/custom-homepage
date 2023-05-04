cfb48fc7d596680bcd3e527adf7c48be59b2c098
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-05-03 22:14:53 -0700

    Add websocket functionality for updating areas

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,11 +1,19 @@
 class Area < ApplicationRecord
   has_many :web_links, dependent: :destroy
   after_create_commit { broadcast_create_area }
+  after_update_commit { broadcast_update_area }
   after_destroy_commit { broadcast_destroy_area }
 
   private
   def broadcast_create_area
     ActionCable.server.broadcast('AreasChannel', { action: 'create', broadcasted_area: {
+      id:,
+      name:,
+    }})
+  end
+
+  def broadcast_update_area
+    ActionCable.server.broadcast('AreasChannel', { action: 'update', broadcasted_area: {
       id:,
       name:,
     }})
