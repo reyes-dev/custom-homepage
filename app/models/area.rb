d80b178f50681a8ff3989555562af07d8f1e2171
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-05-03 22:13:42 -0700

    Add websocket functionality for deleting areas

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,10 +1,18 @@
 class Area < ApplicationRecord
   has_many :web_links, dependent: :destroy
   after_create_commit { broadcast_create_area }
+  after_destroy_commit { broadcast_destroy_area }
 
   private
   def broadcast_create_area
     ActionCable.server.broadcast('AreasChannel', { action: 'create', broadcasted_area: {
+      id:,
+      name:,
+    }})
+  end
+
+  def broadcast_destroy_area
+    ActionCable.server.broadcast('AreasChannel', { action: 'destroy', broadcasted_area: {
       id:,
       name:,
     }})
